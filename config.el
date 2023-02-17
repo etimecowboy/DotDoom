489e41f431b9ac8be59e1869dff4e5d6ef484010
Xin <etimecowboy@gmail.com>
2023-02-17 12:29:27 +0800

    Add treemacs config.

--- config.el
+++ config.el
@@ -77,3 +77,19 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; treemacs
+(map! "M-0" #'treemacs-select-window)
+
+(after! treemacs
+  ;; opens/closes files using ace
+  (treemacs-define-RET-action 'file-node-closed #'treemacs-visit-node-ace)
+  (treemacs-define-RET-action 'file-node-open #'treemacs-visit-node-ace))
+
+(add-hook! 'treemacs-node-hook
+           #'(lambda ()
+                ;; (message "treemacs-mode-hook `%s'" (current-buffer))
+                ;; NOTE: Treemacs buffer in terminal mode cannot display
+                ;; scaled text.
+                (when window-system
+                  (text-scale-decrease 1))))
